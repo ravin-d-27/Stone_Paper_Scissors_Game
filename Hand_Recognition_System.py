f3942e374f89882cec7524d2050ca870ac6deb24
ravin-d-27 <ravin.d3107@outlook.com>
2023-09-08 22:57:28 +0530

    Added the Second version of experimentation

--- Hand_Recognition_System.py
+++ Hand_Recognition_System.py
@@ -1,53 +1,79 @@
-
 import cv2
-
 from ML_model import ML_Selector
-
 import HandTracker as ht
+from game import Stone_Paper_Scissors
+import time
 
-wcam,hcam = 640,480
+wcam, hcam = 640, 480
 cap = cv2.VideoCapture(0)
-cap.set(3,wcam) 
-cap.set(5,hcam) 
+cap.set(3, wcam)
+cap.set(5, hcam)
 
 detector = ht.Hand_Detector()
 
-tips = [4,8,12,16,20]
+tips = [4, 8, 12, 16, 20]
 model = ML_Selector("hands_data.csv")
 model.run()
 
-while True:
+player = 0
+bot = 0
+
+vals = {
+    2: "stone",
+    1: "paper",
+    0: "scissors"
+}
+
+lock = 1
+while lock:
     success, img = cap.read()
     img = detector.Discover_Hand(img)
     locs = detector.location(img)
-    #print(locs)
-    
-    if len(locs)!=0:
+
+    cv2.putText(img, "Player: {}".format(player), (10, 70), cv2.FONT_HERSHEY_PLAIN, 3, (0, 0, 255), 2)
+    cv2.putText(img, "Bot: {}".format(bot), (10, 120), cv2.FONT_HERSHEY_PLAIN, 3, (0, 0, 255), 2)
+
+    if len(locs) != 0:
         fingers = []
-        
-        if locs[tips[0]][1] > locs[tips[0]-1][1]:
+
+        if locs[tips[0]][1] > locs[tips[0] - 1][1]:
             fingers.append(1)
         else:
             fingers.append(0)
-            
-        for tip in range(1,5):
-            if locs[tips[tip]][2] < locs[tips[tip]-2][2]:
+
+        for tip in range(1, 5):
+            if locs[tips[tip]][2] < locs[tips[tip] - 2][2]:
                 fingers.append(1)
             else:
                 fingers.append(0)
-        
+
         val = model.find(fingers)
-        if val[0] == 0:
-            cv2.putText(img,"Thumbs Up!",(10,70),cv2.FONT_HERSHEY_PLAIN,3,(255,0,255),3)
-        elif val[0] == 1:
-            cv2.putText(img,"Scissors!",(10,70),cv2.FONT_HERSHEY_PLAIN,3,(0,0,255),3)
+
+        obj = Stone_Paper_Scissors()
+
+        if val[0] == 1:
+            points = obj.play(val[0] - 1)
+            print(points)
+            cv2.putText(img, "You: Scissors  Bot: {}".format(vals[points[2]]), (10, 200), cv2.FONT_HERSHEY_PLAIN, 3,
+                        (255, 0, 0), 2)
+            time.sleep(1)
         elif val[0] == 2:
-            cv2.putText(img,"Paper!",(10,70),cv2.FONT_HERSHEY_PLAIN,3,(255,0,0),3)
+            points = obj.play(val[0] - 1)
+            print(points)
+            cv2.putText(img, "You: Paper  Bot: {}".format(vals[points[2]]), (10, 200), cv2.FONT_HERSHEY_PLAIN, 3,
+                        (255, 0, 0), 2)
+            time.sleep(1)
         elif val[0] == 3:
-            cv2.putText(img,"Rock!",(10,70),cv2.FONT_HERSHEY_PLAIN,3,(0,255,0),3)
+            points = obj.play(val[0] - 1)
+            print(points)
+            cv2.putText(img, "You: Stone  Bot: {}".format(vals[points[2]]), (10, 200), cv2.FONT_HERSHEY_PLAIN, 3,
+                        (255, 0, 0), 2)
+            time.sleep(1)
         else:
             pass
-            
+
     cv2.imshow("Image",img)
     cv2.waitKey(1)
 
+cap.release()
+cv2.destroyAllWindows()
--- game.py
+++ game.py
@@ -4,29 +4,29 @@ class Stone_Paper_Scissors():
 
     def __init__(self):
         self.vals = {
-            0:"stone",
+            2:"stone",
             1:"paper",
-            2:"scissors"
+            0:"scissors"
         }
 
     def play(self, h_ans):
         # [bot_ans, h_ans]
-        ans = random.randint(0,2)
-        bot_ans = self.vals[ans]
-
+        bot_ans = random.randint(0,2)
         if (bot_ans == h_ans ):
-            return [0,0] 
+            return [0,0,bot_ans] 
         else:
-            if (bot_ans == 0 and h_ans == 1):
-                return [0,1]
-            elif (bot_ans == 0 and h_ans == 2):
-                return [1,0]
-            elif (bot_ans == 1 and h_ans == 0):
-                return [1,0]
-            elif (bot_ans == 2 and h_ans == 0):
-                return [0,1]
-            elif (bot_ans == 1 and h_ans == 2):
-                return [0,1]
-            elif (bot_ans == 2 and h_ans == 1):
-                return [1,0]
+            if (bot_ans == 0 and h_ans == 1): # Scissors and paper
+                return [1,0,bot_ans]
+            elif (bot_ans == 0 and h_ans == 2): # Scissors and Stone
+                return [0,1, bot_ans]
+            elif (bot_ans == 1 and h_ans == 0): # Paper and Scissors
+                return [0,1,bot_ans]
+            elif (bot_ans == 2 and h_ans == 0): # Stone and Scissors
+                return [1,0,bot_ans]
+            elif (bot_ans == 1 and h_ans == 2): # Paper and Stone
+                return [1,0,bot_ans]
+            elif (bot_ans == 2 and h_ans == 1): # Stone and Paper
+                return [0,1,bot_ans]
+            else:
+                pass
 
