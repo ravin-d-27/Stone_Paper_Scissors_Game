a90622711eee80402289ed9f643abc90df6bd540
ravin-d-27 <ravin.d3107@outlook.com>
2023-09-08 23:24:41 +0530

    The Game is Ready!

--- Hand_Recognition_System.py
+++ Hand_Recognition_System.py
@@ -15,21 +15,23 @@ tips = [4, 8, 12, 16, 20]
 model = ML_Selector("hands_data.csv")
 model.run()
 
-player = 0
-bot = 0
-
 vals = {
     2: "stone",
     1: "paper",
     0: "scissors"
 }
+player = 0
+bot = 0
 
-lock = 1
-while lock:
+print()
+x = int(input("Enter the No. of Points: "))
+
+while (player<x and bot<x):
     success, img = cap.read()
     img = detector.Discover_Hand(img)
     locs = detector.location(img)
 
+    
     cv2.putText(img, "Player: {}".format(player), (10, 70), cv2.FONT_HERSHEY_PLAIN, 3, (0, 0, 255), 2)
     cv2.putText(img, "Bot: {}".format(bot), (10, 120), cv2.FONT_HERSHEY_PLAIN, 3, (0, 0, 255), 2)
 
@@ -53,27 +55,62 @@ while lock:
 
         if val[0] == 1:
             points = obj.play(val[0] - 1)
-            print(points)
-            cv2.putText(img, "You: Scissors  Bot: {}".format(vals[points[2]]), (10, 200), cv2.FONT_HERSHEY_PLAIN, 3,
-                        (255, 0, 0), 2)
-            time.sleep(1)
+
+            print("You: Scissors  \nBot: {}\n\n".format(vals[points[2]]))
+
+            time.sleep(2)
+            player+=points[1]
+            bot+=points[0]
+            cap.release()
+            cv2.destroyAllWindows()
+            cap = cv2.VideoCapture(0)
+            cap.set(3, wcam)
+            cap.set(5, hcam)
+            continue
+
         elif val[0] == 2:
             points = obj.play(val[0] - 1)
-            print(points)
-            cv2.putText(img, "You: Paper  Bot: {}".format(vals[points[2]]), (10, 200), cv2.FONT_HERSHEY_PLAIN, 3,
-                        (255, 0, 0), 2)
-            time.sleep(1)
+
+            print("You: Paper  \nBot: {}\n\n".format(vals[points[2]]))
+            time.sleep(2)
+            player+=points[1]
+            bot+=points[0]
+            cap.release()
+            cv2.destroyAllWindows()
+            cap = cv2.VideoCapture(0)
+            cap.set(3, wcam)
+            cap.set(5, hcam)
+            continue
+
         elif val[0] == 3:
             points = obj.play(val[0] - 1)
-            print(points)
-            cv2.putText(img, "You: Stone  Bot: {}".format(vals[points[2]]), (10, 200), cv2.FONT_HERSHEY_PLAIN, 3,
-                        (255, 0, 0), 2)
-            time.sleep(1)
-        else:
-            pass
+            print("You: Stone  \nBot: {}\n\n".format(vals[points[2]]))
+            
+            time.sleep(2)
+            player+=points[1]
+            bot+=points[0]
+            cap.release()
+            cv2.destroyAllWindows()
+            cap = cv2.VideoCapture(0)
+            cap.set(3, wcam)
+            cap.set(5, hcam)
+            continue
 
-    cv2.imshow("Image",img)
+        if (player==x or bot==x):
+            break
+
+    cv2.imshow("Image", img)
     cv2.waitKey(1)
 
-cap.release()
-cv2.destroyAllWindows()
+if (bot>player):
+    print("You Lost, Bot won the Match!")
+    print("Your Score: ", player)
+    print("Bot Score: ", bot)
+elif (bot==player):
+    print("Its a Tie game")
+    print("Your Score: ", player)
+    print("Bot Score: ", bot)
+else:
+    print("Congratulations! You Won the game!")
+    print("Your Score: ", player)
+    print("Bot Score: ", bot)
